a68c811d99f5a2478e37f01ccbfcb8f21ea73a95
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2020-11-13 10:56:20 +0200

    Bumps deps & add prettier.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,157 +1,138 @@
 const path = require('path');
-const glob = require("glob");
+const glob = require('glob');
+const { argv } = require('yargs');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 
-const mode = process.env.NODE_ENV === 'development' ? 'development' : 'production';
-const stats = mode === 'development' ? 'errors-warnings' : { children: false };
-
+const isDev = argv.mode === 'development';
+const stats = isDev ? 'errors-warnings' : { children: false };
 
 module.exports = {
-    mode: mode,
     stats: stats,
     entry: glob.sync('./src/components/**/*.js').reduce((acc, path) => {
-        const entry = path.replace(/^.*[\\\/]/, '').replace('.js','');
+        const entry = path.replace(/^.*[\\\/]/, '').replace('.js', '');
         acc[entry] = path;
         return acc;
-      }, {}),
+    }, {}),
     output: {
         filename: './assets/bundle.[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new BundleAnalyzerPlugin({
-            analyzerMode: 'disabled',
-            generateStatsFile: true
-        }),
-        new MiniCssExtractPlugin({
-            filename: '/assets/bundle.[name].css'
-        }),
-        new CopyPlugin([{
-                from: 'src/components/**/*',
-                to: '[folder]/[name].[ext]',
-                ignore: [
-                    'src/components/**/*.js',
-                    'src/components/**/*.scss',
-                    'src/assets/**/*',
-                    'src/components/sections/**/*',
-                    'src/components/templates/**/*',
-                    'src/components/snippets/**/*',
-                    'src/components/tailwind.css'
-                ]
-            },
-            {
-                from: 'src/components/templates/**/*.liquid',
-                to: 'templates/[name].[ext]',
-                flatten: true
-            },            
-            {
-                from: 'src/components/sections/**/*.liquid',
-                to: 'sections/[name].[ext]',
-                flatten: true
-            },
-            {
-                from: 'src/components/snippets/**/*.liquid',
-                to: 'snippets/[name].[ext]',
-                flatten: true
-            },
-            {
-                from: 'src/assets/**/*',
-                to: 'assets/',
-                flatten: true
-            },
-            {
-                from: 'src/config/*.json',
-                to: 'config/[name].[ext]',
-            },
-            {
-                from: 'src/locales/*.json',  
-                to: 'locales/[name].[ext]', 
-            },
-        ])
-    ],
     module: {
-        rules: [{
+        rules: [
+            {
                 test: /\.(sc|sa|c)ss$/,
                 use: [
                     MiniCssExtractPlugin.loader,
                     {
                         loader: 'css-loader',
                         options: {
-                            url: false
-                        }
+                            url: false,
+                        },
                     },
                     'postcss-loader',
                     {
                         loader: 'sass-loader',
                         options: {
                             sourceMap: true,
-                        }
-                    }
-                ]
+                        },
+                    },
+                ],
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
+                use: ['file-loader'],
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['transform-class-properties'],
-                        plugins: ['babel-plugin-transform-class-properties']
-                    }
-                }
+                use: ['file-loader'],
             },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    }
-};
+                loader: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        isDev &&
+            new BundleAnalyzerPlugin({
+                analyzerMode: 'disabled',
+                generateStatsFile: true,
+            }),
+        isDev &&
+            new WebpackShellPluginNext({
+                onBuildStart: {
+                    scripts: ['echo -- Webpack build started 🛠'],
+                    blocking: true,
+                    parallel: false,
+                },
+                onBuildError: {
+                    scripts: ['echo -- ☠️ Aw snap, Webpack build failed...'],
+                },
+                onBuildEnd: {
+                    scripts: [
+                        'echo -- Webpack build complete ✓',
+                        'echo -- Building TailwindCSS...',
+                        'npx tailwindcss build src/components/tailwind.css -o dist/assets/tailwind.css.liquid',
+                        'echo -- Deploying to theme ✈️',
+                        'shopify-themekit deploy',
+                        'echo -- Deployment competed ✓',
+                        'shopify-themekit open',
+                        'shopify-themekit watch',
+                    ],
+                    blocking: true,
+                    parallel: false,
+                },
+            }),
 
-// Run Shell commmands during Webpack operations
-if (mode === 'development') {
-    module.exports.plugins.push(
-      new WebpackShellPluginNext({
-        onBuildStart:{
-          scripts: ['echo -- Webpack build started 🛠'],
-          blocking: true,
-          parallel: false
-        },
-        onBuildError:{
-            scripts: ['echo -- ☠️ Aw snap, Webpack build failed...'],
-        }, 
-        onBuildEnd:{
-          scripts: [
-              'echo -- Webpack build complete ✓',
-              'echo -- Building TailwindCSS...',
-              'npx tailwindcss build src/components/tailwind.css -o dist/assets/tailwind.css.liquid',
-              'echo -- Deploying to theme ✈️',
-              'shopify-themekit deploy',
-              'echo -- Deployment competed ✓',
-              'shopify-themekit open',
-              'shopify-themekit watch'],
-          blocking: true,
-          parallel: false
-        }
-      })
-    )
-  }
+        new MiniCssExtractPlugin({
+            filename: '/assets/bundle.[name].css',
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'src/components/*/*.liquid',
+                    to: '[folder]/[name].[ext]',
+                },
+                {
+                    from: 'src/components/templates/**/*.liquid',
+                    to: 'templates/[name].[ext]',
+                    flatten: true,
+                },
+                {
+                    from: 'src/components/sections/**/*.liquid',
+                    to: 'sections/[name].[ext]',
+                    flatten: true,
+                },
+                {
+                    from: 'src/components/snippets/**/*.liquid',
+                    to: 'snippets/[name].[ext]',
+                    flatten: true,
+                },
+                {
+                    from: 'src/assets/**/*',
+                    to: 'assets/',
+                    flatten: true,
+                },
+                {
+                    from: 'src/config/*.json',
+                    to: 'config/[name].[ext]',
+                },
+                {
+                    from: 'src/locales/*.json',
+                    to: 'locales/[name].[ext]',
+                },
+            ],
+        }),
+    ].filter(Boolean),
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+};
