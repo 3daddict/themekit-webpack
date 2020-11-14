d24c1a8b8e0f336fcb93fa025ce6cc9aaa63aa75
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2020-11-14 13:48:47 +0200

    Initial HMR support

--- src/components/snippets/message/message.js
+++ src/components/snippets/message/message.js
@@ -1,6 +1,6 @@
 // testing importing scss
-import "./message.scss";
+import './message.scss';
 
 // Testing arrow functions
-const messageFunction = () => 'Message JS ES6 Function';
+const messageFunction = () => 'Message JS ES6 Function with HMR!!!';
 console.log(messageFunction());
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path');
 const glob = require('glob');
 const { argv } = require('yargs');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 
-const isDev = argv.mode === 'development';
-const stats = isDev ? 'errors-warnings' : { children: false };
+const isDevMode = argv.mode === 'development';
+const stats = isDevMode ? 'errors-warnings' : { children: false };
+const port = 9000;
+const publicPath = isDevMode ? `https://localhost:${port}/` : '';
 
 module.exports = {
     stats: stats,
@@ -19,14 +20,18 @@ module.exports = {
     }, {}),
     output: {
         filename: './assets/bundle.[name].js',
+        hotUpdateChunkFilename: './hot/[id].[fullhash].hot-update.js',
+        hotUpdateMainFilename: './hot/[fullhash].hot-update.json',
         path: path.resolve(__dirname, 'dist'),
+        publicPath,
     },
+    cache: false,
     module: {
         rules: [
             {
                 test: /\.(sc|sa|c)ss$/,
                 use: [
-                    MiniCssExtractPlugin.loader,
+                    isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
                     {
                         loader: 'css-loader',
                         options: {
@@ -58,18 +63,18 @@ module.exports = {
         ],
     },
     plugins: [
-        new CleanWebpackPlugin(),
-        isDev &&
-            new BundleAnalyzerPlugin({
-                analyzerMode: 'disabled',
-                generateStatsFile: true,
-            }),
-        isDev &&
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false,
+        }),
+        isDevMode &&
             new WebpackShellPluginNext({
                 onBuildStart: {
-                    scripts: ['echo -- Webpack build started 🛠'],
-                    blocking: true,
-                    parallel: false,
+                    scripts: [
+                        'echo -- Webpack build started 🛠',
+                        'shopify-themekit watch --env=development',
+                    ],
+                    blocking: false,
+                    parallel: true,
                 },
                 onBuildError: {
                     scripts: ['echo -- ☠️ Aw snap, Webpack build failed...'],
@@ -80,10 +85,9 @@ module.exports = {
                         'echo -- Building TailwindCSS...',
                         'npx tailwindcss build src/components/tailwind.css -o dist/assets/tailwind.css.liquid',
                         'echo -- Deploying to theme ✈️',
-                        'shopify-themekit deploy',
+                        'shopify-themekit deploy --env=development',
                         'echo -- Deployment competed ✓',
                         'shopify-themekit open',
-                        'shopify-themekit watch',
                     ],
                     blocking: true,
                     parallel: false,
@@ -91,28 +95,41 @@ module.exports = {
             }),
 
         new MiniCssExtractPlugin({
-            filename: '/assets/bundle.[name].css',
+            filename: './assets/bundle.[name].css',
         }),
         new CopyPlugin({
             patterns: [
                 {
-                    from: 'src/components/*/*.liquid',
+                    from: 'src/components/**/*.liquid',
                     to: '[folder]/[name].[ext]',
-                },
-                {
-                    from: 'src/components/templates/**/*.liquid',
-                    to: 'templates/[name].[ext]',
                     flatten: true,
-                },
-                {
-                    from: 'src/components/sections/**/*.liquid',
-                    to: 'sections/[name].[ext]',
-                    flatten: true,
-                },
-                {
-                    from: 'src/components/snippets/**/*.liquid',
-                    to: 'snippets/[name].[ext]',
-                    flatten: true,
+                    transformPath(targetPath, absolutePath) {
+                        const relativePath = path.join(__dirname, 'src/components');
+                        const diff = path.relative(relativePath, absolutePath);
+                        const targetFolder = diff.split(path.sep)[0];
+                        return path.join(targetFolder, path.basename(absolutePath));
+                    },
+                    transform: isDevMode
+                        ? function (content) {
+                              content = content
+                                  .toString()
+                                  .replace(
+                                      /{{\s*'([^']+)'\s*\|\s*asset_url\s*\|\s*(stylesheet_tag|script_tag)\s*}}/g,
+                                      function (matched, fileName, type) {
+                                          if (type === 'stylesheet_tag') {
+                                              if (fileName !== 'tailwind.css') {
+                                                  return '';
+                                              }
+                                              return matched;
+                                          }
+
+                                          return `<script src="${publicPath}assets/${fileName}"></script>`;
+                                      }
+                                  );
+
+                              return content;
+                          }
+                        : undefined,
                 },
                 {
                     from: 'src/assets/**/*',
@@ -132,7 +149,19 @@ module.exports = {
     ].filter(Boolean),
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+        },
         compress: true,
-        port: 9000,
+        port,
+        https: true,
+        disableHostCheck: true,
+        hot: true,
+        liveReload: false,
+        overlay: true,
+        writeToDisk: true,
     },
 };
