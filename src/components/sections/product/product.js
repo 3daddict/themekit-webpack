4465d8eda7e7a82c1db5ff1d64547b3125b6c5b7
3daddict <print@3daddict.com>
2021-01-20 18:34:52 -0800

    es lint version bump

--- src/components/sections/product/product.js
+++ src/components/sections/product/product.js
@@ -1,7 +1,7 @@
 import {post} from '../../helpers/cart-fetch-api/cart-fetch-api';
 
 document.getElementById('AddToCartForm').onsubmit = async function (event) {
-    const btn = document.getElementById('AddToCartBtn')
+    const btn = document.getElementById('AddToCartBtn');
     const id = document.getElementById('AddToCartBtn').value;
     const option1 = document.getElementById('option1').value;
     const option2 = document.getElementById('option2').value;
@@ -11,7 +11,7 @@ document.getElementById('AddToCartForm').onsubmit = async function (event) {
     const response = await post('add.js', {id, option1, option2, qty});
     
     if(response) {
-        btn.textContent = 'ITEM ADDED'
+        btn.textContent = 'ITEM ADDED';
     }
     return false;
 };
